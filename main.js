bfc1296adb698dd145f9700232dcdb99bf2f36b0
Carlos Roman <carlochess@gmail.com>
2016-12-01 22:53:58 -0500

    Fix

--- main.js
+++ main.js
@@ -1,59 +1,76 @@
-// HTCondor.js
-var soap = require("soap");
- 
-function HTCondor(config){
-   if (!(this instanceof HTCondor)) {
-    return new HTCondor(config);
-   }
-   var self = this
-   self.config = config;
-}
- 
- 
-HTCondor.prototype.createSchedduler = function (cb){
-   soap.createClient(config.wsdl, {},
-     function(err, client){
-        if (err) {
-            cb(err);
-        } else {
-        cb(null,new Schedd(client));
-        }
-   },config.url);
-}
+var HTCondor = require("./index.js")
+var join = require("path").join
 
-HTCondor.prototype.createCollector = function (cb){
-   soap.createClient(config.wsdl, {},
-     function(err, client){
-        if (err) {
-            cb(err);
-        } else {
-        cb(null,new Collector(client));
-        }
-   },config.url);
-}
+var job = {
+    universe: "VANILLA",
+    executable: "/bin/sleep",
+    arguments: "31",
+    notification: "never",
+    owner: "rooot",
+    type: "5",
+    requirements: 'TRUE',
+    shouldtransferfiles: "yes",
+    when_to_transfer_output: "ON_EXIT",
+    out: "example1.out",
+    err: "example1.err",
+    log: "example1.log",
+    iwd: "/tmp",
+    queue: 1
+};
 
-// main.js
+var dockerJob = {
+    universe: "VANILLA",
+    dockerimage: "haskell",
+    executable: "/usr/bin/ruby",
+    arguments: '-e "puts 2+2"',
+    notification: "never",
+    owner: "rooot",
+    type: "5",
+    requirements: 'TRUE',
+    shouldtransferfiles: "no",
+    when_to_transfer_output: "ON_EXIT",
+    out: "example1.out",
+    err: "example1.err",
+    log: "example1.log",
+    iwd: "/tmp",
+    queue: 1
+};
 
-var htcondor = new HTCondor({url:"http://localhost:8080/", wsdl : "wsdl/condorSchedd.wsdl"});
+var dagJob = {
+    dagLocation: join(__dirname, "test", "dag", "dagman.dag"),
+    owner: "carlos"
+};
+
+
+var htcondor = new HTCondor({url:"http://localhost:8080/", wsdl : join(__dirname, "wsdl", "condorSchedd.wsdl")});
 
 htcondor.createSchedduler(function(err, schedd){
    if(err){
-    console.log("Error al crear Schedd")
+    console.log("Error al crear Schedd", err)
     return;
    }
-   
-   schedd.createJob({}, function(err, job){
-       console.log(job)
-   })
-})
 
-htcondor.createCollector(function(err, coll){
-   if(err){
-    console.log("Error al crear Collec")
-    return;
-   }
-   
-   coll.createJob({}, function(err, job){
+   /*schedd.createJob(job, function(err, job){
+       if(err){
+         console.log("Error al enviar job",err)
+         return
+       }
+       console.log(job)
+   })*/
+
+   schedd.createDagJob(dagJob, function(err, job){
+       if(err){
+         console.log("Error al enviar dag",err)
+         return
+       }
        console.log(job)
    })
+
+   /*schedd.createDockerJob(dockerJob, function(err, job){
+       if(err){
+         console.log("Error al enviar dag",err)
+         return
+       }
+       console.log(job)
+   })*/
 })
